eb3f7cc500720855b04af4b3f2b60172482f6bf9
itforget CORDEIRO <itforget11@gmail.com>
2024-04-10 18:33:07 +0000

    add: favoritos

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
-const express = require('express')
+const express = require("express")
+const rotaLivro = require("./rotas/livro")
+const rotaFavorito = require("./rotas/favorito")
+
+const cors = require("cors")
+
 const app = express()
-const rotaLivro = require('./rotas/livro')
+app.use(express.json())
+app.use(cors({origin: "*"}))
 
 app.use('/livros', rotaLivro)
+app.use('/favoritos', rotaFavorito)
 
 const port = 8000
-
-app.get('/', (req, res) => {
-    res.send('Olá Mundo!')
-})
-
-app.listen(port, () => {
-    console.log(`Escutando a porta ${port}`)
-})
--- rotas/livro.js
+++ rotas/livro.js
@@ -1,19 +1,16 @@
-const { Router } = require('express')
-const  { getLivros, getLivro } = require('../controladores/livros')
+const { Router } = require("express")
+const { getLivros, getLivro, postLivro, patchLivro, deleteLivro } = require("../controladores/livro")
+
 const router = Router()
 
 router.get('/', getLivros)
 
 router.get('/:id', getLivro)
 
-router.post('/', (req, res) =>{
-    res.send('Você fez uma requisição do tipo POST')
-})
-router.patch('/', (req, res) =>{
-    res.send('Você fez uma requisição do tipo PATCH')
-})
-router.delete('/', (req, res) =>{
-    res.send('Você fez uma requisição do tipo DELETE')
-})
+router.post('/', postLivro)
+
+router.patch('/:id', patchLivro)
+
+router.delete('/:id', deleteLivro)
 
 module.exports = router
